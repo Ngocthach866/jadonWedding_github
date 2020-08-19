f835919bbc96f9f998db8e60286aa2bc28b51929
ijimae118 <58478046+ijimae118@users.noreply.github.com>
2020-08-19 17:22:49 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('Ven', function () {
-    return view('Ven');
-});
-Route::get('Pro', function () {
-    return view('Pro');
-});
-Route::get('Pcs', function () {
-    return view('Pcs');
-});
-Route::get('Pct', function () {
-    return view('Pct');
-});
-Route::get('Pts', function () {
-    return view('Pts');
-});
+
